a7fe0735aadc28430436c13ef4246453c73a6c94
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-01 16:07:10 +0530

    done accordain, completed

--- Animated FAQ Section/script.js
+++ Animated FAQ Section/script.js
@@ -4,7 +4,14 @@ accordians.forEach((accordian) => {
     const answer = accordian.querySelector('.answer');
     const icon = accordian.querySelector('.icon');
     accordian.addEventListener('click', () => {
-        answer.classList.toggle('active');
-        icon.classList.toggle('active');
+        // answer.classList.toggle('active');
+        // icon.classList.toggle('active');
+        if(icon.classList.contains('active')){
+            icon.classList.remove('active');
+            answer.style.maxHeight = null;
+        }else {
+            icon.classList.add('active');
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+        }
     })
 })
