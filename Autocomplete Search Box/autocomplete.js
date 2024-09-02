078d521690c33f325cb7848ac5796e8cbb4cd99e
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-02 22:28:40 +0530

    Add scroll bar and completed the autocomplete searchbox

--- Autocomplete Search Box/autocomplete.js
+++ Autocomplete Search Box/autocomplete.js
@@ -7,6 +7,26 @@ let availableKeywords = [
     'Where to learn coding online',
     'Where to learn web design',
     'How to create a website',
+    "How to tie a tie",
+    "What is love?",
+    "Who is the richest person in the world?",
+    "How to lose weight",
+    "What is the meaning of life?",
+    "How to make money online",
+    "When is the next holiday?",
+    "What is the weather forecast?",
+    "How to cook [dish name]",
+    "What is the capital of [country]?",
+    "How to change a tire",
+    "How to learn a new language",
+    "What is the best way to learn to play guitar?",
+    "How to start a business",
+    "How to write a resume",
+    "What is the best way to study for a test?",
+    "How to make a YouTube channel",
+    "How to write a book",
+    "What is the best way to save money?",
+    "How to find a job",
 ];
 
 const resultBox = document.querySelector('.result-box');
@@ -15,20 +35,20 @@ const inputBox = document.getElementById('input-box');
 const button = document.querySelector('button');
 
 
-inputBox.onkeyup = function() {
+inputBox.onkeyup = function () {
     // resultBox.classList.remove('hide');
     let result = [];
     let input = inputBox.value;
-    if(input.length){
+    if (input.length) {
         result = availableKeywords.filter((keyword) => {
-            if(input === ' '){
+            if (input === ' ') {
                 return;
             }
             return keyword.toLocaleLowerCase().includes(input.toLocaleLowerCase());
         });
     };
     display(result);
-    if(!result.length){
+    if (!result.length) {
         resultBox.innerHTML = '';
     }
 };
@@ -45,11 +65,12 @@ function display(result) {
     });
 
     // solution using regExp:
-    // console.log(content.join(',').replace(new RegExp(',', "g"), ''));
+    // console.log(content.join(',').replace(new RegExp(',', "g",), ''));
     // const pureContent = content.join(',').replace(/,/g, '');
 
     // most easy
     resultBox.innerHTML = '<ul>' + content.join('') + '</ul>';
+
     // if(result.length === 0){
     //     resultBox.innerHTML = ''
     // }
