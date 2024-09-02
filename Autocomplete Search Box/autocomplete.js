f29ec8785af41849a1409ad5f1b0b33f048bc0b3
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-02 21:39:02 +0530

    Search options display where , is not shown

--- Autocomplete Search Box/autocomplete.js
+++ Autocomplete Search Box/autocomplete.js
@@ -9,16 +9,37 @@ let availableKeywords = [
     'How to create a website',
 ];
 
-const resultBox = document.querySelector('result-box');
+const resultBox = document.querySelector('.result-box');
 const inputBox = document.getElementById('input-box');
+const ul = document.querySelector('.result-box ul');
+const button = document.querySelector('button');
+
 
 inputBox.onkeyup = function() {
     let result = [];
     let input = inputBox.value;
     if(input.length){
         result = availableKeywords.filter((keyword) => {
+            if(input === ' '){
+                return;
+            }
             return keyword.toLocaleLowerCase().includes(input.toLocaleLowerCase());
         });
     };
-    return result;
+    console.log(result);
+    display(result)
 };
+
+function display(result) {
+    const content = result.map((search) => {
+        return '<li>' + search + '</li>'
+    });
+
+    // solution using regExp:
+    // console.log(content.join(',').replace(new RegExp(',', "g"), ''));
+    // const pureContent = content.join(',').replace(/,/g, '');
+
+    // most easy
+    ul.innerHTML = content.join('');
+};
+
