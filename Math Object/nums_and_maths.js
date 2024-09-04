1046631ae8683de5e971577377c34e79d9365778
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-04 22:57:29 +0530

    numbers in Js is done

---
+++ Math Object/nums_and_maths.js
@@ -0,0 +1,27 @@
+// const score = 400.706637;
+// const score = 407.7262;
+
+// console.log(typeof score);
+
+// toString()
+// console.log(typeof score.toString());
+
+// Decimal lagake kitne tak numbers chahiye as an arguement pass karo toFixed() main
+// console.log(score.toFixed(2))
+
+// console.log(score.toPrecision(9));// The integer arguement passed in toPrecision can be greater  but the interget number value should not be greater orelse an exponential useless number will be returned which would break our code
+
+const hundred = 1000000;
+// console.log(hundred.toLocaleString());// seperated the number with commas in US standard by default
+
+// console.log(hundred.toLocaleString('en-IN'));// Indian number separater is 'en-IN'
+
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.NaN)
+console.log(Number.NEGATIVE_INFINITY)
+console.log(Number.POSITIVE_INFINITY)
+
+
