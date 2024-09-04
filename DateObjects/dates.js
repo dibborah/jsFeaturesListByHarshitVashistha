45d3b2190734c7e801a0b367ceab44da541ce35d
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-04 21:37:56 +0530

    date object is started. Methods are practiced like: getDate, getHours, etc.

---
+++ DateObjects/dates.js
@@ -0,0 +1,22 @@
+// // Date 
+
+// let myDate = new Date();
+
+// // same
+// console.log('myDate', myDate);
+// // console.log('toString()', myDate.toString());
+
+// console.log('getDate()', myDate.getDate());
+// console.log('getDay', myDate.getDay());
+// console.log('getHours()', myDate.getHours());
+// console.log('getMilliseconds', myDate.getMilliseconds());
+// console.log('getMinutes', myDate.getMinutes());
+// console.log('getMonth', myDate.getMonth());
+// console.log('getFullYear', myDate.getFullYear());
+// console.log('getTime', myDate.getTime());
+
+
+// console.log('toDateString', myDate.toDateString());// without slash
+// console.log('toDateString', myDate.toLocaleString());// with slash
+
+// console.log('type of Date constructor is:',typeof myDate);
